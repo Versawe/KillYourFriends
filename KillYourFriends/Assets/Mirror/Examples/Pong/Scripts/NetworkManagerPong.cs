611ce4768abf445c5dad060c7b2c1237158847f6
Eric Versaw <ericversaw22@gmail.com>
2021-09-02 20:19:46 -0400

    Pong Example
    Experimented with the pong example for a bit. Mirror works well, but tcp transport stinks for that game.

--- KillYourFriends/Assets/Mirror/Examples/Pong/Scripts/NetworkManagerPong.cs
+++ KillYourFriends/Assets/Mirror/Examples/Pong/Scripts/NetworkManagerPong.cs
@@ -1,40 +1,39 @@
 using UnityEngine;
+using Mirror;
 
-namespace Mirror.Examples.Pong
+
+// Custom NetworkManager that simply assigns the correct racket positions when
+// spawning players. The built in RoundRobin spawn method wouldn't work after
+// someone reconnects (both players would be on the same side).
+[AddComponentMenu("")]
+public class NetworkManagerPong : NetworkManager
 {
-    // Custom NetworkManager that simply assigns the correct racket positions when
-    // spawning players. The built in RoundRobin spawn method wouldn't work after
-    // someone reconnects (both players would be on the same side).
-    [AddComponentMenu("")]
-    public class NetworkManagerPong : NetworkManager
+    public Transform leftRacketSpawn;
+    public Transform rightRacketSpawn;
+    GameObject ball;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
     {
-        public Transform leftRacketSpawn;
-        public Transform rightRacketSpawn;
-        GameObject ball;
+        // add player at correct spawn position
+        Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
 
-        public override void OnServerAddPlayer(NetworkConnection conn)
+        // spawn ball if two players
+        if (numPlayers == 2)
         {
-            // add player at correct spawn position
-            Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
-            GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
-            NetworkServer.AddPlayerForConnection(conn, player);
-
-            // spawn ball if two players
-            if (numPlayers == 2)
-            {
-                ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
-                NetworkServer.Spawn(ball);
-            }
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            NetworkServer.Spawn(ball);
         }
+    }
 
-        public override void OnServerDisconnect(NetworkConnection conn)
-        {
-            // destroy ball
-            if (ball != null)
-                NetworkServer.Destroy(ball);
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        // destroy ball
+        if (ball != null)
+            NetworkServer.Destroy(ball);
 
-            // call base functionality (actually destroys the player)
-            base.OnServerDisconnect(conn);
-        }
+        // call base functionality (actually destroys the player)
+        base.OnServerDisconnect(conn);
     }
 }
--- KillYourFriends/Assets/Mirror/Examples/Pong/Scripts/Player.cs
+++ KillYourFriends/Assets/Mirror/Examples/Pong/Scripts/Player.cs
@@ -1,19 +1,16 @@
 using UnityEngine;
-
-namespace Mirror.Examples.Pong
+using Mirror;
+public class Player : NetworkBehaviour
 {
-    public class Player : NetworkBehaviour
-    {
-        public float speed = 30;
-        public Rigidbody2D rigidbody2d;
+    public float speed = 30;
+    public Rigidbody2D rigidbody2d;
 
-        // need to use FixedUpdate for rigidbody
-        void FixedUpdate()
-        {
-            // only let the local player control the racket.
-            // don't control other player's rackets
-            if (isLocalPlayer)
-                rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
-        }
+    // need to use FixedUpdate for rigidbody
+    void FixedUpdate()
+    {
+        // only let the local player control the racket.
+        // don't control other player's rackets
+        if (isLocalPlayer)
+            rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
     }
 }
